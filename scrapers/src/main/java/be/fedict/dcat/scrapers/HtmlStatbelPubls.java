88b05ec9bf3d4f62a34e47705f7c9a8a374eb32b
Bart Hanssens <bart.hanssens@fedict.be>
2015-11-06 20:08:47 +0100

    Correction for URIs

--- scrapers/src/main/java/be/fedict/dcat/scrapers/HtmlStatbelPubls.java
+++ scrapers/src/main/java/be/fedict/dcat/scrapers/HtmlStatbelPubls.java
@@ -28,7 +28,6 @@ package be.fedict.dcat.scrapers;
 
 import be.fedict.dcat.helpers.Cache;
 import be.fedict.dcat.helpers.Storage;
-import be.fedict.dcat.vocab.DCAT;
 import be.fedict.dcat.vocab.MDR_LANG;
 import java.io.File;
 import java.io.IOException;
