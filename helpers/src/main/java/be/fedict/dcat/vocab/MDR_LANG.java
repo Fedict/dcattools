207a03d86c69c7a2f5489a0357a180b22bf2cbc4
Bart Hanssens <bart.hanssens@fedict.be>
2015-11-06 17:50:17 +0100

    Added EPO MDR language

---
+++ helpers/src/main/java/be/fedict/dcat/vocab/MDR_LANG.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015, Bart Hanssens <bart.hanssens@fedict.be>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package be.fedict.dcat.vocab;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.ValueFactoryImpl;
+
+
+/**
+ * EU Publication Office Metadata Registry, Languages
+ * 
+ * @author Bart Hanssens <bart.hanssens@fedict.be>
+ */
+public class MDR_LANG {
+    public static final String NAMESPACE = 
+                "http://publications.europa.eu/resource/authority/language";
+    
+    public final static String PREFIX = "mdrlang";
+    
+    public final static URI DE;
+    public final static URI EN;
+    public final static URI FR;
+    public final static URI NL;
+    
+    public final static Map<URI, String> map;
+    
+    static {
+	ValueFactory factory = ValueFactoryImpl.getInstance();
+        
+        DE = factory.createURI(MDR_LANG.NAMESPACE, "DEU");
+        EN = factory.createURI(MDR_LANG.NAMESPACE, "ENG");
+        FR = factory.createURI(MDR_LANG.NAMESPACE, "FRA");
+        NL = factory.createURI(MDR_LANG.NAMESPACE, "NLD");
+        
+        map = new HashMap<>();
+        map.put(DE, "de");
+        map.put(EN, "en");
+        map.put(FR, "fr");
+        map.put(NL, "nl");
+        
+    }
+}
