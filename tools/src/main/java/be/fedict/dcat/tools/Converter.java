b5cb5b64349d886b85205f3fb33f39ac28ad4e55
Bart Hanssens <bart.hanssens@fedict.be>
2016-04-13 10:16:32 +0200

    Added converter tool

---
+++ tools/src/main/java/be/fedict/dcat/tools/Converter.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2016, Bart Hanssens <bart.hanssens@fedict.be>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package be.fedict.dcat.tools;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFWriter;
+import org.openrdf.rio.Rio;
+import org.openrdf.sail.memory.MemoryStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Serialization format converter (e.g. N-Triples to RDF/XML)
+ * 
+ * @author Bart Hanssens <bart.hanssens@fedict.be>
+ */
+public class Converter {
+    private final static Logger logger = LoggerFactory.getLogger(LinkChecker.class);
+    
+    private final static String PROP_PREFIX = "be.fedict.dcat.tools.linkchecker";
+
+    /**
+     * Main program
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        logger.info("-- START --");
+        if (args.length < 2) {
+            logger.error("No input or output file");
+            System.exit(-1);
+        }
+        
+        Optional<RDFFormat> fmtin = Rio.getParserFormatForFileName(args[0]);
+        if(!fmtin.isPresent()) {
+            logger.error("No parser for input {}", args[0]);
+            System.exit(-2);
+        }
+        
+        Optional<RDFFormat> fmtout = Rio.getWriterFormatForFileName(args[1]);
+        if(!fmtout.isPresent()) {
+            logger.error("No parser for output {}", args[1]);
+            System.exit(-3);            
+        }
+        
+        int code = 0;
+        Repository repo = new SailRepository(new MemoryStore());
+	repo.initialize();
+        
+        try (RepositoryConnection con = repo.getConnection()) {
+            con.add(new File(args[0]), "http://data.gov.be", fmtin.get());
+            RDFWriter writer = Rio.createWriter(fmtout.get(),
+                        new FileOutputStream(new File(args[1])));
+            con.export(writer);
+        } catch (IOException ex) {
+            logger.error("Error converting", ex);
+            code = -1;
+        }
+        
+        repo.shutDown();
+        
+        System.exit(code);
+    }    
+}
