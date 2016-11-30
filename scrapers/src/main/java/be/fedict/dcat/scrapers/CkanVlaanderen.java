b5f99467b7c7dd9b71ad5bbadbe9b6960ed75202
Bart Hanssens <bart.hanssens@fedict.be>
2016-11-30 10:35:39 +0100

    Scrape temporal from JSON vlaanderen Signed-off-by:Bart Hanssens <bart.hanssens@fedict.be>

--- scrapers/src/main/java/be/fedict/dcat/scrapers/CkanVlaanderen.java
+++ scrapers/src/main/java/be/fedict/dcat/scrapers/CkanVlaanderen.java
@@ -55,8 +55,9 @@ public class CkanVlaanderen extends CkanJson {
     public final static String DOMAIN = "beleidsdomein";
     public final static String FREQ = "frequency-of-update";
     public final static String GEMET = "gemet-theme";
-    public final static String GEOCOVERAGE = "geografische dekking";
+    public final static String GEOCOVERAGE = "Geografische Dekking";
     public final static String METADATA_REQ = "metadata_request";
+	public final static String TIMECOVERAGE = "Dekking in Tijd";
     public final static String RESPONSABLE = "responsible-party";
     
     /**
@@ -88,6 +89,9 @@ public class CkanVlaanderen extends CkanJson {
                     break;
                 case CkanVlaanderen.GEOCOVERAGE:
                     parseString(store, uri, obj, CkanJson.VALUE, DCTERMS.COVERAGE, lang);
+                    break;
+				case CkanVlaanderen.TIMECOVERAGE:
+                    parseString(store, uri, obj, CkanJson.VALUE, DCTERMS.TEMPORAL, lang);
                     break;
                 case CkanVlaanderen.METADATA_REQ:
                     parseURI(store, uri, obj, CkanJson.VALUE, DCAT.LANDING_PAGE);
