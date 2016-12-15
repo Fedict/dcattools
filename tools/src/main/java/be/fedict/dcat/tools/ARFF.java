3840fd102e1a1769fec6b454ada29bceab62c94c
Bart Hanssens <bart.hanssens@fedict.be>
2016-12-15 15:47:15 +0100

    Splitting classifier into ARFF generator and classifier (todo) Signed-off-by:Bart Hanssens <bart.hanssens@fedict.be>

--- tools/src/main/java/be/fedict/dcat/tools/ARFF.java
+++ tools/src/main/java/be/fedict/dcat/tools/ARFF.java
@@ -24,7 +24,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-package be.fedict.dcat.classifier;
+package be.fedict.dcat.tools;
 
 import be.fedict.dcat.helpers.Storage;
 
@@ -54,18 +54,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Dataset theme classifier
+ * Generate ARFF for machine learning tools.
  * 
  * @author Bart.Hanssens
  */
-public class Main {
-	private final static Logger logger = LoggerFactory.getLogger(Main.class);
+public class ARFF {
+	private final static Logger logger = LoggerFactory.getLogger(ARFF.class);
 
-	private final static String PROP_PREFIX = "be.dcat.classifier";
+	private final static String PROP_PREFIX = "be.fedict.dcat.tools.arff";
 	private final static Properties prop = new Properties();
 
-	private final static Pattern WHITES = Pattern.compile("\\s");
-	private final static Pattern ALPHA = Pattern.compile("\\w");
+	private final static Pattern WHITES = Pattern.compile("\\s+");
+	private final static Pattern ALPHA = Pattern.compile("\\W+");
 	
 	private static Storage store = null;
 
@@ -90,8 +90,8 @@ public class Main {
      * Read RDF file.
      */
     private static void readRDF() {
-        String file = prop.getProperty(PROP_PREFIX + ".store");
-        store = new Storage(new File(file));       
+        String file = prop.getProperty(PROP_PREFIX + ".store", "");
+        store = new Storage(new File(file));
         try {
             store.startup();
         } catch (RepositoryException ex) {
@@ -99,7 +99,7 @@ public class Main {
             exit(-3);
         }
         
-        String rdfin = prop.getProperty(PROP_PREFIX + ".rdfin");
+        String rdfin = prop.getProperty(PROP_PREFIX + ".rdfin", "");
         try {
             store.read(new BufferedReader(new FileReader(rdfin)));
         } catch (IOException ex ) {
@@ -127,23 +127,42 @@ public class Main {
 	}
 
 	/**
-	 * Sanitize a list of strings
+	 * Sanitize list of strings
 	 * 
-	 * @param str input string
+	 * @param strs array of input string
 	 * @return lowercase string without punctuation
 	 */
 	private static String sanitize(List<String> strs) {
 		if (strs == null || strs.isEmpty()){
 			return "";
 		}
-		String s = "";
 		
-		for(String str: strs) {
+		String s = "";
+		for (String str: strs) {
 			String tmp = WHITES.matcher(str).replaceAll(" ");
-			tmp = ALPHA.matcher(tmp).replaceAll(" ");
-			s += tmp + " ";
+			s += ALPHA.matcher(tmp).replaceAll(" ");
 		}
 		return s.toLowerCase();
+	}
+	
+	/**
+	 * Turn a list of themes into boolean strings
+	 * 
+	 * @param dataset list of dataset themes
+	 * @param all list of all possible themes
+	 * @return comma-separated string of booleans
+	 */
+	private static String[] bitThemes(List<String> dataset, String[] all) {
+		dataset.removeIf(str -> !str.startsWith("http://publications.europa.eu"));
+		dataset.replaceAll(str -> str.substring(str.lastIndexOf("/") + 1, str.lastIndexOf("/") + 5));
+		
+		String[] bools = new String[all.length];
+		int i = 0;
+		for (String theme: all) {
+			bools[i++] = dataset.contains(theme) ? "1" : "0";
+		}
+		
+		return bools;
 	}
 	
 	
@@ -152,18 +171,34 @@ public class Main {
 	 * 
 	 * @param w output writer
 	 * @param langs languages
+	 * @param themes DCAT themes
 	 * @throws IOException 
 	 */
-	private static void writeARFFHeader(BufferedWriter w, String[] langs) throws IOException {
+	private static void writeARFFHeader(BufferedWriter w, String[] langs, String[] themes) 
+													throws IOException {
+		w.append("@relation 'DCAT datasets:")
+				.append(" -C -").append(String.valueOf(themes.length)).append("'");
+		w.newLine();
+		w.newLine();
+		
 		w.append("@attribute id string");
+		w.newLine();
 		for (String lang: langs) {
 			w.append("@attribute title").append(lang).append(" string");
+			w.newLine();
 			w.append("@attribute desc").append(lang).append(" string");
+			w.newLine();
 			w.append("@attribute keywords").append(lang).append(" string");
+			w.newLine();
 		}
-		w.newLine();
+		for (String theme: themes) {
+			w.append("@attribute ").append(theme).append(" {0,1}");
+			w.newLine();
+		}
+
 		w.newLine();
 		w.append("@data");
+		w.newLine();
 		w.newLine();
 	}
 	
@@ -172,20 +207,33 @@ public class Main {
 	 * 
 	 * @param w output writer
 	 * @param langs languages
+	 * @param themes DCAT themes
 	 * @param uri IRI of the dataset
 	 * @throws IOException 
 	 */
-	private static void writeARFFLine(BufferedWriter w, String[] langs, IRI uri) throws IOException {		
+	private static void writeARFFLine(BufferedWriter w, String[] langs, String[] themes, IRI uri) 
+															throws IOException {		
 		Map<IRI, ListMultimap<String, String>> fields = store.queryProperties(uri);
+		
+		w.append(uri.stringValue()).append(',');
+		
 		for (String lang : langs) {
 			String title = sanitize(Storage.getOne(fields, DCTERMS.TITLE, lang));
 			String desc = sanitize(Storage.getOne(fields, DCTERMS.DESCRIPTION, lang));
 			String kw = sanitize(Storage.getMany(fields, DCAT.KEYWORD, lang));
-			String themes = sanitize(Storage.getMany(fields, DCAT.THEME, ""));
 
-			w.append(title).append(',')
-					.append(desc).append(',')
-					.append(kw);
+			w.append('\'').append(title).append('\'').append(',')
+					.append('\'').append(desc).append('\'').append(',')
+					.append('\'').append(kw).append('\'').append(',');
+		}
+		
+		String[] themebits = bitThemes(Storage.getMany(fields, DCAT.THEME, ""), themes);
+		int i = 1;
+		for (String themebit: themebits) {
+			w.append(themebit);
+			if (i++ < themebits.length) {
+				w.append(',');
+			}
 		}
 		w.newLine();
 	}
@@ -205,24 +253,30 @@ public class Main {
 			logger.error("No languages defined");
 			exit(-6);
 		}
+
+		String[] themes = prop.getProperty(PROP_PREFIX + ".themes", "").split(",");
+		if (themes.length == 0 || themes[0].isEmpty()) {
+			logger.error("No themes defined");
+			exit(-7);
+		}
 		
 		try (BufferedWriter w = Files.newBufferedWriter(Paths.get(file))) {
-			writeARFFHeader(w, langs);
+			writeARFFHeader(w, langs, themes);
 		
 			List<IRI> uris = store.query(DCAT.DATASET);
 			for(IRI uri: uris) {
-				writeARFFLine(w, langs, uri);
+				writeARFFLine(w, langs, themes, uri);
 			}
 			w.close();
 		} catch (IOException ex) {
 			logger.error("Error writing to arff", ex);
-			exit(-7);
+			exit(-8);
 		}
 	}
     
 		
 	/**
-     * Main program
+     * ARFF program
      * 
      * @param args 
      */
