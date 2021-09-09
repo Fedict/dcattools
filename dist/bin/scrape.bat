4a1446985e55102a75e62e7280e102a4cd1304a4
Bart Hanssens <bart.hanssens@fedict.be>
2021-09-09 18:53:40 +0100

    More refactoring, move queries to resource jars Signed-off-by:Bart Hanssens <bart.hanssens@fedict.be>

--- dist/bin/scrape.bat
+++ dist/bin/scrape.bat
@@ -1,1 +1,1 @@
-java -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dorg.slf4j.simpleLogger.logFile=scrape-%1.log -jar scrapers.jar  B:/datagov/cfg/%1/scraper.properties
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=debug -Dorg.slf4j.simpleLogger.logFile=scrape-%1.log -jar scrapers.jar  %1
