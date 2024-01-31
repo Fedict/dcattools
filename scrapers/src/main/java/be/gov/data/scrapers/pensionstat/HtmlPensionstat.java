c85a9d505c1091ff105aa10f7990f16ddae8c175
Bart Hanssens <bart.hanssens@bosa.fgov.be>
2024-01-31 22:24:19 +0100

    GH-201: add landingPage

--- scrapers/src/main/java/be/gov/data/scrapers/pensionstat/HtmlPensionstat.java
+++ scrapers/src/main/java/be/gov/data/scrapers/pensionstat/HtmlPensionstat.java
@@ -227,6 +227,8 @@ public class HtmlPensionstat extends Html {
 			store.add(dataset, DCTERMS.LANGUAGE, MDR_LANG.MAP.get(lang));
 			store.add(dataset, DCTERMS.TITLE, title, lang);
 			store.add(dataset, DCTERMS.DESCRIPTION, desc, lang);
+			store.add(dataset, DCAT.LANDING_PAGE, p.getUrl());
+			store.add(store.getURI(p.getUrl().toString()), DCTERMS.LANGUAGE, MDR_LANG.MAP.get(lang));
 
 			Element download = doc.selectFirst(DOWNLOADS);
 			if (download != null) {
